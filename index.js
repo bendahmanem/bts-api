c61477a6698120885356745c9f3d5304f947155e
Mounir <mounir.bendahmane@ecole-isitech.fr>
2023-06-07 11:56:15 +0200

    bodyparser added

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const postRoutes = require("./routes/post");
 
 const app = express();
 
+// place un middleware qui permet de parser le
+// contenu du body des requetes entrantes
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   console.log("je suis un middleware ");
   req.test = "hello";
-  //next();
+  next();
 });
 
 app.use((req, res, next) => {
@@ -16,6 +21,9 @@ app.use((req, res, next) => {
 });
 
 app.use("/blog", postRoutes);
+
+// donnez l'acces a l'application aux routes qui permettent de gerer les produits
+// creez un fichier route et un controller qui correspond
 
 app.listen(3003, () => {
   console.log("App is listening on port 3003");
